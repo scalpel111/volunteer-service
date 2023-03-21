de80623ebb71ddba056c58ab8b5a5a32198ea5aa
scalpel111 <2826444973@qq.com>
2023-03-21 16:36:02 +0800

    init

---
+++ volunteer/src/main/resources/volunteer_service.sql
@@ -0,0 +1,151 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80028
+ Source Host           : localhost:3306
+ Source Schema         : volunteer_service
+
+ Target Server Type    : MySQL
+ Target Server Version : 80028
+ File Encoding         : 65001
+
+ Date: 21/03/2023 16:20:38
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for activity
+-- ----------------------------
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE `activity`  (
+  `activity_id` int(0) NOT NULL AUTO_INCREMENT,
+  `theme` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `start_time` datetime(0) NOT NULL,
+  `end_time` datetime(0) NOT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `require` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `discription` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `recruit_number` int(0) NOT NULL,
+  `tip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`activity_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of activity
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for institution
+-- ----------------------------
+DROP TABLE IF EXISTS `institution`;
+CREATE TABLE `institution`  (
+  `institution_id` int(0) NOT NULL AUTO_INCREMENT,
+  `institution_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `admin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `admin _id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `tellphone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `proof` blob NOT NULL,
+  `count` int(0) NULL DEFAULT 0,
+  PRIMARY KEY (`institution_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of institution
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for institution_activity
+-- ----------------------------
+DROP TABLE IF EXISTS `institution_activity`;
+CREATE TABLE `institution_activity`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `institution_id` int(0) NOT NULL,
+  `activity_id` int(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `institution_id`(`institution_id`) USING BTREE,
+  INDEX `activity_id`(`activity_id`) USING BTREE,
+  CONSTRAINT `institution_activity_ibfk_1` FOREIGN KEY (`institution_id`) REFERENCES `institution` (`institution_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `institution_activity_ibfk_2` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`activity_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of institution_activity
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for root
+-- ----------------------------
+DROP TABLE IF EXISTS `root`;
+CREATE TABLE `root`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `admin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `passowrd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of root
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` int(0) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `identify_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `identify_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `tellphone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `service_time` double NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'scalpel', '111111', '620422200202101910', '刘谊', '17309435144', 2.4);
+INSERT INTO `user` VALUES (2, 'lyic', '111111', NULL, NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for user_activity
+-- ----------------------------
+DROP TABLE IF EXISTS `user_activity`;
+CREATE TABLE `user_activity`  (
+  `activity_id` int(0) NOT NULL,
+  `user_id` int(0) NOT NULL,
+  `status` int(0) NULL DEFAULT NULL COMMENT '0(已报名),1(未开始),2(进行中),3(已完成)',
+  INDEX `user_id`(`user_id`) USING BTREE,
+  INDEX `activity_id`(`activity_id`) USING BTREE,
+  CONSTRAINT `user_activity_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_activity_ibfk_2` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`activity_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_activity
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_institution
+-- ----------------------------
+DROP TABLE IF EXISTS `user_institution`;
+CREATE TABLE `user_institution`  (
+  `user_id` int(0) NOT NULL,
+  `institution_id` int(0) NOT NULL,
+  `status` int(0) NOT NULL DEFAULT 0 COMMENT '0(普通用户),1(管理员)',
+  INDEX `user_id`(`user_id`) USING BTREE,
+  INDEX `institution_id`(`institution_id`) USING BTREE,
+  CONSTRAINT `user_institution_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_institution_ibfk_2` FOREIGN KEY (`institution_id`) REFERENCES `institution` (`institution_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_institution
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
