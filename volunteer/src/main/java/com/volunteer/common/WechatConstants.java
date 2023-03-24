836e8b944ca1a6fb771107c2637bf37c08b80afd
scalpel111 <2826444973@qq.com>
2023-03-24 23:28:55 +0800

    回来了

--- volunteer/src/main/java/com/volunteer/common/WechatConstants.java
+++ volunteer/src/main/java/com/volunteer/common/WechatConstants.java
@@ -1,6 +1,6 @@
 package com.volunteer.common;
 
 public class  WechatConstants {
-    public static final String APPID = "wx6aaba25c9c51477e";
-    public static final String APP_SECRET = "923bf280085e02015a99cbb8cc19818c";
+    public static final String APPID = "";
+    public static final String APP_SECRET = "";
 }
