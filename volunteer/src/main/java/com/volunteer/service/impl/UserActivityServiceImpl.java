df06cdf2c86e1ef9017577f63b9033347ecb4506
scalpel111 <2826444973@qq.com>
2023-04-10 18:59:09 +0800

    2.1.2

--- volunteer/src/main/java/com/volunteer/service/impl/UserActivityServiceImpl.java
+++ volunteer/src/main/java/com/volunteer/service/impl/UserActivityServiceImpl.java
@@ -40,8 +40,6 @@ public class UserActivityServiceImpl extends ServiceImpl<UserActivityMapper, Use
     @Resource
     private ActivityService activityService;
 
-    @Resource
-    private UserActivityService userActivityService;
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
